0a4e1d092ee1d64ac8c0e2da36436d367bf2fb6a
kazuto mituhashi <potopiajp@yahoo.co.jp>
2013-01-09 09:56:46 +0900

    10→20

--- CL_K04_1/CL_K04_1.c
+++ CL_K04_1/CL_K04_1.c
@@ -1,12 +1,12 @@
 /*1T1_31_O‹´˜al*/
 /*‰Û‘è04_1*/
 #include <stdio.h>
-
+//
 void main(void){
 	int a[5];
 	int i;
 	for(i = 0;i < 5;i = i + 1){
-		a[i] = i * 10;
+		a[i] = i * 20;
 	}
 	for(i = 0;i < 5;i = i + 1){
 		printf("a[%d] = %d\n",i,a[i]);
